952dcf4b16e5298c80050812ba4b8560ff619b9e
Will Sparger <spargerw@gmail.com>
2016-12-22 17:37:36 -0600

    created matches html and styled it

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
+
   var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
     auth: {
       redirectUrl: AUTH0_CALLBACK_URL,
       responseType: 'token',
       params: { scope: 'openid email' } //Details: https://auth0.com/docs/scopes
-
     }
   });
 
@@ -17,50 +17,52 @@ $(document).ready(function() {
     e.preventDefault();
     logout();
   })
+  //after completing quiz, generate profile need to tie to body becuase SPA loads .generate_profile button late
+  $('body').on('click', '.generate_profile', function(e) {
+    generateProfile();
+    e.preventDefault();
+  });
+
 
   lock.on("authenticated", function(authResult) {
-    alert('called');
     //set token in localStorage after authenticated,
     localStorage.setItem('id_token', authResult.idToken);
 
     lock.getProfile(authResult.idToken, function(error, profile) {
 
       if (error) {
-        // Handle error
         console.error(error);
         return;
       }
       console.log('profile: ', profile);
-
       //checkPrefs(profile)
       console.log('authResult.idToken', authResult.idToken);
 
-
       retrieve_profile();
       console.log(retrieve_profile());
-      // Display user information
-      // show_profile_info(profile);
-      //check p
+
       checkPreference(profile);
     });
   });
 
-function loadQuiz(){
-  console.log('loadQuiz');
-  $('#result').load('/muttmatch_frontend/questions.html #main');
-};
+  var loadQuiz = function(){
+    console.log('load Quiz');
+    $('#result').load('/questions.html #main');
+  };
 
-//Show dogs
-var checkPreference = function( profile ){
+  var loadProfile = function(){
+    console.log('load Profile');
+    $('#result').load('/matches.html');
+  };
+
+  var checkPreference = function( profile ){
     //access database with get request to backend (ajax) using jwt to veryify good connection
     var idToken = localStorage.getItem('id_token');
     console.log('new token: ', idToken);
 
     var request = $.ajax({
-
-      url: 'https://radiant-waters-50602.herokuapp.com/checkprefs', //we wouldn't want to hardcode this
+      url: 'https://radiant-waters-50602.herokuapp.com/checkprefs', //we shouldn't hardcode this
       method: 'POST',
-
       //need to send authorization header
       headers: {
         'Authorization': 'Bearer ' + idToken
@@ -70,40 +72,156 @@ var checkPreference = function( profile ){
         userid: profile.user_id
       }
     });
-
+//THIS ISNT WORKING RIGHT
     request.done(function(results){
       console.log('results: ',results);
       if (results) {
-        //if we have a profile
+        //if we have a profile, load matches.html
+        loadProfile();
       }
       else {
         loadQuiz();
-        //if we don't have a profile redirect to questionnaire
-
-        // alert("We don't have your profile! Take our quick quiz to set your preferences!");
-        // var questionnaire = [
-        //   "<section class='create-account'><h1>Create Account</h1> </br><p>Let's start off with the basics</p></br></br><h2>Your Living Situation</h2></br><p>Some shelters have restrictions based on age and residence type. We want to ensure thematches we show are available for you.<br>We also need to know if you have restrictions on pet types, allergies, and what kind of pet you’re looking for.</p>",
-        //   "  <br>We also need to know if you have restrictions on pet types, allergies, and what kind of pet you’re looking for.</p></br></br>",
-        //   "<div class='container'><div class='boxes'><div class='col-xs-8 col-xl-4'><p2>Any allergies?</p><br><label><input type='checkbox' name='restrictions'>Allergic to dogs</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='restrictions'>Allergic to cats</label></div></div>",
-        //   "<div class='boxes'><div class='col-xs-8 col-xl-4'><p2>Who lives with you?</p><br><label><input type='checkbox' name='restrictions'>I have children</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='restrictions'>I have a dog</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='restrictions'>I have a cat</label></div></div>",
-        //   "<div class='boxes'><div class='col-xs-12'></br><p2>Residence Type?</p><br><div class='col-xs-4'><label><input type='radio' name='residence'>House</label></div><div class='col-xs-4'><label><input type='radio' name='residence'>Apartment (no weight restriction)</label></div><div class='col-xs-4'><label><input type='radio' name='residence'>Apartment (weight restriction)</label></div></div></div></section>",
-        //
-        //   "<section class='create-account'><h1>Your Activity</h1></br><p>Tell us how you like to spend your time (select all that apply)</p></br><p>I like to...</p></br>",
-        //   "<div class='container'><div class='boxes'><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='activity'>Sleep all the time</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='activity'>Snuggle on the couch</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='activity'>Play outside 24/7</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='activity'>Go running/jogging/swimming</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='activity'>I have a cat</label></div></div></div>",
-        //   "<div class='pet'><div class='col-xs-8 col-xl-4'><p3>I am looking for a (select all that apply)</p></br><label><input type='checkbox' name='restrictions'>Dog</label></div><div class='col-xs-8 col-xl-4'><label><input type='checkbox' name='restrictions'>Cat</label></div></div></section>",
-        //   "<section class='buttons'><div class='container'><div class='row'><div class='col-xs-6 col-xl-4'><a href='question_one.html' class='sign_up btn btn-primary btn-lg active' role='button' aria-pressed='true'>Back</a></div><div class='col-xs-6 col-xl-4'><a href='question_two.html' class='sign_up btn btn-primary btn-lg active' role='button' aria-pressed='true'>See Results</a></div></div></div></section>"
-        // ]
-        // $('#container').append(questionnaire);
-
       }
-      // for (var i = 0, x = results.length; i<x; i++){
-      //   $('.main_ul').append('<li>' + results[i].size + '</li>');
-      // }
     });
   };
 
+  var generateProfile = function(){
+    //generate Profile object with all values set to false
+    var Profile = {
+      active: false,
+      lazy: false,
+      dog_allergy:false ,
+      cat_allergy: false,
+      has_kids: false,
+      has_dogs: false,
+      has_cats: false,
+      home_size: false,
+      want_dog: false,
+      want_cat: false
+    }
+    //access idToken because call requires jwt token
+    var idToken = localStorage.getItem('id_token');
+    var $profile_value = $('.boxes').find('input');
+    //set values of Profile keys corresponding to survey answers
+    $profile_value.each(function(i,box){
+      box = $(box);
+      switch(box.val()){
+        case 'Allergic to dogs':
+          Profile.dog_allergy = box.is(':checked');
+        break;
+        case 'Allergic to cats':
+          Profile.cat_allergy = box.is(':checked');
+        break;
+        case 'I have children':
+          Profile.has_kids = box.is(':checked');
+        break;
+        case 'I have a dog':
+          Profile.has_dogs = box.is(':checked');
+        break;
+        case 'I have a cat':
+          Profile.has_cats = box.is(':checked');
+        break;
+        case 'Sleep all the time':
+          Profile.lazy = box.is(':checked');
+        break;
+        case 'Snuggle on the couch':
+          Profile.lazy = box.is(':checked');
+        break;
+        case 'Play outside 24/7':
+          Profile.active = box.is(':checked');
+        break;
+        case 'Go running/jogging/swimming':
+          Profile.active = box.is(':checked');
+        break;
+        case 'Dog':
+          Profile.want_dog = box.is(':checked');
+        break;
+        case 'Cat':
+          Profile.want_cat = box.is(':checked');
+        break;
+        case 'House':
+          if (box.is(':checked')){
+            Profile.home_size = "large";
+          }
+          else {Profile.home_size = "small"}
+        break;
+      }
+    });
 
-  //retrieve the profile:
+    console.log('Final profile: ', Profile);
+    var request = $.ajax({
+      url: 'https://radiant-waters-50602.herokuapp.com/userpreference',
+      method: 'POST',
+      // need to send authorization header
+      headers: {
+        'Authorization': 'Bearer ' + idToken
+      },
+      data: Profile
+    });
+    request.done(function(results){ //
+      console.log('results: ',results);
+      if (results) {
+        console.log('results: ', results);
+        //showdogs(results) function
+        showDogs(results);
+      }
+      else {
+        console.log('results did not exist')
+      }
+    });
+  };
+  //show dogs function
+  var showDogs = function(profile){
+    loadProfile();
+    var idToken = localStorage.getItem('id_token');
+    //use profile here to find matching dogs
+
+    var request = $.ajax({
+      // $('#matches-list').empty(); //ensure contents of html are cleared
+      url: 'https://radiant-waters-50602.herokuapp.com/findmatch',
+      method: 'POST',
+      // need to send authorization header for security
+      headers: {
+        'Authorization': 'Bearer ' + idToken
+      },
+      data: profile
+    });
+    request.done(function(results){ //
+      if (results) {
+        console.log('match results: ', results);
+        //loop through matches to grab image name and breed values
+        for (var i = 0; i < results.length; i++){
+          var image = results[i].image;
+          var name = results[i].name;
+          var breed = results[i].breed;
+          //insert to display
+          var elements = [
+          '<li>',
+            '<div class="row">',
+              '<div class="col-xs-4 match-image">',
+                '<a href=#>',
+                  '<img src=',image,'>',
+                '</a>',
+              '</div>',
+              '<div class="col-xs-4 match-info">',
+                '<h5>', name, '</h5>',
+              '</div>',
+              '<div class="col-xs-4 match-info">',
+                '<h5>', breed, '</h5>',
+              '</div>',
+            '</div>',
+          '</li>'
+        ].join('');
+        $('#matches-list').append(elements);
+        }
+      }
+      else {
+        console.log('results did not exist')
+      }
+    });
+
+  };
+
   var retrieve_profile = function() {
     var id_token = localStorage.getItem('id_token');
     console.log('id_token is: ', id_token);
@@ -111,7 +229,7 @@ var checkPreference = function( profile ){
       console.log('we have it');
       lock.getProfile(id_token, function (err, profile) {
         if (err) {
-          return alert('There was an error getting the profile: ' + err.message);
+          console.error('There was an error getting the profile: ' + err.message);
         }
         // Display user information
         show_profile_info(profile);
@@ -131,6 +249,5 @@ var checkPreference = function( profile ){
     localStorage.removeItem('id_token');
     window.location.href = "/";
   };
-
   retrieve_profile();
 });
